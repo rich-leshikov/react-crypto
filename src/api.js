b998ff474736ae99fe5a63a9fefa3880b53cc065
Richard Leshik <richardleshik@gmail.com>
2024-01-27 11:20:16 +0300

    feat: add fake API functions

---
+++ src/api.js
@@ -0,0 +1,17 @@
+import { cryptoAssets, cryptoData } from '@/data.js'
+
+export const fakeFetchCrypto = () => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(cryptoData)
+    }, 2000)
+  })
+}
+
+export const fetchAssets = () => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(cryptoAssets)
+    }, 2000)
+  })
+}
