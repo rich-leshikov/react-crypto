465fd83d824733e26f1847db024203b2fcb4096b
Richard Leshik <richardleshik@gmail.com>
2024-01-29 19:07:37 +0300

    feat: add assets cards

--- src/components/layout/AppSider.tsx
+++ src/components/layout/AppSider.tsx
@@ -2,26 +2,52 @@ import React, { useEffect, useState } from 'react'
 
 import { fakeFetchCrypto, fetchAssets } from '@/api'
 import { CryptoAssets, CryptoDataResult } from '@/data'
-import { percentDifference } from '@/utils'
+import { capitalize, percentDifference } from '@/utils'
 import { ArrowDownOutlined, ArrowUpOutlined } from '@ant-design/icons'
-import { Card, Layout, List, Spin, Statistic, Typography } from 'antd'
+import { Card, Layout, List, Spin, Statistic, Tag, Typography } from 'antd'
 
 const siderStyle: React.CSSProperties = {
   padding: '1rem',
 }
 
-const data = [
-  'Racing car sprays burning fuel into crowd.',
-  'Japanese princess to wed commoner.',
-  'Australian walks 100km after outback crash.',
-  'Man charged over missing wedding girl.',
-  'Los Angeles battles huge wildfires.',
-]
-
 export const AppSider = () => {
   const [loading, setLoading] = useState<boolean>(false)
   const [crypto, setCrypto] = useState<[] | CryptoDataResult>([])
   const [assets, setAssets] = useState<[] | CryptoAssets>([])
+
+  const assetsCards = assets.map(asset => (
+    <Card key={asset.id} style={{ marginBottom: '1rem' }}>
+      <Statistic
+        precision={2}
+        prefix={asset.grow ? <ArrowUpOutlined /> : <ArrowDownOutlined />}
+        suffix={'$'}
+        title={capitalize(asset.id)}
+        value={asset.totalAmount}
+        valueStyle={{ color: asset.grow ? '#3f8600' : '#cf1322' }}
+      />
+      <List
+        dataSource={[
+          { title: 'Total profit', value: asset.totalProfit, withTag: true },
+          { isPlain: true, title: 'Asset amount', value: asset.amount },
+        ]}
+        renderItem={item => (
+          <List.Item>
+            <span>{item.title}</span>
+            <span>
+              {item.withTag && <Tag color={asset.grow ? 'green' : 'red'}>{asset.growPercent}%</Tag>}
+              {item.isPlain && item.value}
+              {!item.isPlain && (
+                <Typography.Text type={asset.grow ? 'success' : 'danger'}>
+                  {item.value.toFixed(2)}$
+                </Typography.Text>
+              )}
+            </span>
+          </List.Item>
+        )}
+        size={'small'}
+      />
+    </Card>
+  ))
 
   useEffect(() => {
     const preload = async () => {
@@ -53,37 +79,13 @@ export const AppSider = () => {
     preload()
   }, [])
 
+  if (loading) {
+    return <Spin fullscreen />
+  }
+
   return (
     <Layout.Sider style={siderStyle} width={'25%'}>
-      <Card style={{ marginBottom: '1rem' }}>
-        <Statistic
-          precision={2}
-          prefix={<ArrowUpOutlined />}
-          suffix={'%'}
-          title={'Active'}
-          value={11.28}
-          valueStyle={{ color: '#3f8600' }}
-        />
-        <List
-          dataSource={data}
-          renderItem={item => (
-            <List.Item>
-              <Typography.Text mark>[ITEM]</Typography.Text> {item}
-            </List.Item>
-          )}
-          size={'small'}
-        />
-      </Card>
-      <Card>
-        <Statistic
-          precision={2}
-          prefix={<ArrowDownOutlined />}
-          suffix={'%'}
-          title={'Idle'}
-          value={9.3}
-          valueStyle={{ color: '#cf1322' }}
-        />
-      </Card>
+      {assetsCards}
     </Layout.Sider>
   )
 }
