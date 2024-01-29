ca381f75733ac88c752d8057255ef98b5597eaf1
Richard Leshik <richardleshik@gmail.com>
2024-01-29 18:54:19 +0300

    feat: add capitalize first letter util function

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,7 @@
 export const percentDifference = (a: number, b: number) => {
-  return 100 * Math.abs((a - b) / ((a + b) / 2))
+  return +(100 * Math.abs((a - b) / ((a + b) / 2))).toFixed(2)
+}
+
+export const capitalize = (str: string) => {
+  return str.charAt(0).toUpperCase() + str.slice(1)
 }
