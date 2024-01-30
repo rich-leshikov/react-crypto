1adfd3792d87a4b40782efd4e08b760e4f704d86
Richard Leshik <richardleshik@gmail.com>
2024-01-30 18:10:26 +0300

    feat: add currencies Select with keypress-open ability and mock Modal

--- src/components/layout/AppHeader.tsx
+++ src/components/layout/AppHeader.tsx
@@ -1,16 +1,67 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 
-import { Layout } from 'antd'
+import { useCrypto } from '@/context/crypto-context'
+import { Button, Layout, Modal, Select, Space } from 'antd'
 
+/* eslint-disable */
 const headerStyle: React.CSSProperties = {
-  backgroundColor: '#4096ff',
-  color: '#fff',
-  height: 60,
-  lineHeight: '64px',
-  paddingInline: 48,
+  width: '100%',
   textAlign: 'center',
+  height: 60,
+  padding: '1rem',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
 }
+/* eslint-enable */
 
 export const AppHeader = () => {
-  return <Layout.Header style={headerStyle}>Header</Layout.Header>
+  const [select, setSelect] = useState<boolean>(false)
+  const [modal, setModal] = useState<boolean>(true)
+  const { crypto } = useCrypto()
+
+  useEffect(() => {
+    const keypress = (event: KeyboardEvent) => {
+      if (event.key === '/') {
+        setSelect(prev => !prev)
+      }
+    }
+
+    document.addEventListener('keypress', keypress)
+
+    return () => document.removeEventListener('keypress', keypress)
+  }, [])
+
+  const onSelect = (value: string) => {
+    setModal(true)
+  }
+
+  return (
+    <Layout.Header style={headerStyle}>
+      <Select
+        onClick={() => setSelect(prev => !prev)}
+        onSelect={onSelect}
+        open={select}
+        optionRender={options => (
+          <Space>
+            <img alt={options.data.label} src={options.data.icon} style={{ width: '20px' }} />
+            {options.data.label}
+          </Space>
+        )}
+        options={crypto.map(coin => ({
+          icon: coin.icon,
+          label: coin.name,
+          value: coin.id,
+        }))}
+        style={{ width: '250px' }}
+        value={['press / to open']}
+      />
+      <Button type={'primary'}>Add Asset</Button>
+      <Modal footer={null} onCancel={() => setModal(false)} open={modal}>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
+    </Layout.Header>
+  )
 }
