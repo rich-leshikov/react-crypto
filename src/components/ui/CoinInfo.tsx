04a995714b0f99e4d0078985c3a2f86415edb85e
Richard Leshik <richardleshik@gmail.com>
2024-01-31 21:20:11 +0300

    feat: add isWithSymbol property and condition for title

--- src/components/ui/CoinInfo.tsx
+++ src/components/ui/CoinInfo.tsx
@@ -8,11 +8,12 @@ type CoinInfoProps = {
   isWithSymbol?: boolean
 }
 
-export const CoinInfo: FC<CoinInfoProps> = ({ coin }) => {
+export const CoinInfo: FC<CoinInfoProps> = ({ coin, isWithSymbol = false }) => {
   return (
     <Flex align={'center'}>
       <img alt={coin.name} src={coin.icon} style={{ marginRight: '10px', width: '40px' }} />
       <Typography.Title level={2} style={{ margin: '0' }}>
+        {isWithSymbol && `(${coin.symbol}) `}
         {coin.name}
       </Typography.Title>
     </Flex>
